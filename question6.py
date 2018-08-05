d377802579ee0ed5b07aca928a143c0bd3d697d2
Adhiraj2301 <adhirajshetty97@gmail.com>
2018-08-05 22:21:20 +0530

    Write a program to solve the following problem: You have two jugs, a 4-gallon and a 3-gallon. Neither of the jugs has markings on them. There is a pump that can be used to fill the jugs with water. How can you get exactly two gallons of water in the 4 gallon jug?

---
+++ question6.py
@@ -0,0 +1,23 @@
+x=4
+y=3
+m=x
+n=y
+print("\n0 , 0\n")
+while(x!=2):
+    y=0
+    print("\n",x," , ",y,"\n")
+    x=x-n
+    y=n
+    print("\n",x," , ",y,"\n")
+    y=0
+    print("\n",x," , ",y,"\n")
+    y=x
+    x=0
+    print("\n",x," , ",y,"\n")
+    x=m
+    print("\n",x," , ",y,"\n")
+    x=x-(n-y)
+    y=n
+    print("\n",x," , ",y,"\n")
+    
+    
